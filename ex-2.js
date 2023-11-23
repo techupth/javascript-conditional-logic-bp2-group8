58450f2526da776a7bef4b709b93411f75126659
fonytas <kunagit.jb@gmail.com>
2023-11-23 12:21:37 +0700

    Feat Assignment Conditional Logic : finished Assignments

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,21 @@
 //Exercise 2
-let lightBulbStatus = "On";
+let lightBulbStatus = "Broken";
 
 // Start coding here.
+if(lightBulbStatus === "On"){
+    console.log("Light bulb is on.");
+}else if(lightBulbStatus === "Off"){
+    console.log("Light bulb is off.");
+}else if(lightBulbStatus === "Broken"){
+    console.log('Light bulb is broken.');
+}else {
+    console.log("Please choose the correct input (On/Off/Broken");
+};
+
+// lightBulbStatus === "On"
+// ?(console.log("Light Bulb is on."))
+// : lightBulbStatus==="Off"
+// ?(console.log("Light Bulb is off."))
+// : lightBulbStatus === "Broken"
+// ?(console.log("Light bulb is broken"))
+// :(console.log("Please choose the correct input (On/Off/Broken"));
